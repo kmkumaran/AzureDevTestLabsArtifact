88a25aa89db7671dd22734554052492b080cfaed
Muthukumaran Kasiviswanathan <muthuk@microsoft.com>
2017-05-23 09:43:23 +0530

    Create directory hierarchy

---
+++ Artifacts/windows-deploymentgroup/InstallAndConfigureDeploymentGroupAgent.ps1
@@ -0,0 +1,184 @@
+﻿param (
+    [Parameter(Mandatory=$true)]$accountUrl,
+    [Parameter(Mandatory=$true)]$projectName,
+    [Parameter(Mandatory=$true)]$deploymentGroupName,
+    [Parameter(Mandatory=$true)]$userName,
+    [Parameter(Mandatory=$true)]$patToken,
+    [Parameter(Mandatory=$false)]$deploymentAgentTags,
+    [Parameter(Mandatory=$false)]$agentInstallPath,
+    [Parameter(Mandatory=$false)]$agentName
+    
+)
+
+function Test-InstallPrerequisites
+{
+    If(-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] “Administrator”))
+    { 
+        Write-Error "In sufficient previliges. Run command in Administrator PowerShell Prompt"
+    }
+}
+
+function New-AgentPath 
+{
+    [CmdletBinding()]
+    param([Parameter(Mandatory=$false)] [String] [AllowEmptyString()]$agentInstallPath)
+
+    If ([string]::IsNullOrEmpty($agentInstallPath))
+    {
+        $agentInstallPath = "$env:SystemDrive\vstsagent"
+    }
+    
+
+    If (-NOT (Test-Path "$agentInstallPath"))
+    {
+        New-Item -ItemType Directory -Path $agentInstallPath -Force
+    }
+
+    
+    Write-Host "Created deployment agent install directory: $agentInstallPath"
+    return $agentInstallPath
+}
+
+function Get-DeploymentAgentDownloadUrl
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]$accountUrl,
+        [Parameter(Mandatory=$true)]$userName,
+        [Parameter(Mandatory=$true)]$patToken
+        )
+
+    $platform = "win7-x64"
+    $downloadAPIVersion = "3.0-preview.2"
+    [string] $restCallUrl = $accountUrl + ("/_apis/distributedtask/packages/agent/{0}?top=1&api-version={1}" -f $platform,$downloadAPIVersion)
+    Write-Host "Agent Download REST url: $restCallUrl"
+
+    
+    $basicAuth = ("{0}:{1}" -f $userName, $patToken)
+    $basicAuth = [System.Text.Encoding]::UTF8.GetBytes($basicAuth)
+    $basicAuth = [System.Convert]::ToBase64String($basicAuth)
+    $headers = @{Authorization=("Basic {0}" -f $basicAuth)}
+
+    $response = Invoke-RestMethod -Uri $restCallUrl -headers $headers -Method Get -ContentType 'application/json'
+    Write-Host "Agent Download REST response: $response"
+    
+    $downloadUrl = $response.Value[0].downloadUrl
+    Write-Host "Deployment Agent download url: $downloadUrl"
+
+    return $downloadUrl
+}
+
+function Download-DeploymentGroupAgent
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]$accountUrl,
+        [Parameter(Mandatory=$true)]$userName,
+        [Parameter(Mandatory=$true)]$patToken,
+        [Parameter(Mandatory=$true)]$agentInstallPath,
+        [Parameter(Mandatory=$true)]$agentZipFile
+        )
+   
+    $agentZip="$agentInstallPath\$agentZipFile";
+    $downloadUrl = Get-DeploymentAgentDownloadUrl $accountUrl $userName $patToken
+    pushd $agentInstallPath
+
+    #download
+    Write-Host "Begin Agent download"
+    (New-Object Net.WebClient).DownloadFile($downloadUrl, $agentZip);
+    Write-Host "Agent download complete"
+
+    #extract
+    Add-Type -AssemblyName System.IO.Compression.FileSystem;[System.IO.Compression.ZipFile]::ExtractToDirectory($agentZip, "$agentInstallPath");
+    Write-Host "Agent zip exacted in install directory: $agentInstallPath"
+
+    popd
+}
+
+function Configure-DeploymentGroupAgent
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]$accountUrl,
+        [Parameter(Mandatory=$true)]$projectName,
+        [Parameter(Mandatory=$true)]$deploymentGroupName,
+        [Parameter(Mandatory=$true)]$patToken,
+        [Parameter(Mandatory=$false)][String] [AllowEmptyString()]$agentName,
+        [Parameter(Mandatory=$true)]$agentInstallPath,
+        [Parameter(Mandatory=$false)][String] [AllowEmptyString()]$deploymentAgentTags
+        )
+
+    If ([string]::IsNullOrEmpty($agentName))
+    {
+        $agentName = "$env:COMPUTERNAME"
+    }
+    Write-Host "Configuring Agent: $agentName"
+
+    pushd $agentInstallPath
+    if ([string]::IsNullOrEmpty($deploymentAgentTags)) 
+    {
+        .\config.cmd --deploymentgroup --agent $agentName --runasservice --work '_work' --url $accountUrl --projectname $projectName --deploymentgroupname $deploymentGroupName --auth PAT --token $patToken --unattended 
+    } else {
+        .\config.cmd --deploymentgroup --agent $agentName --runasservice --work '_work' --url $accountUrl --projectname $projectName --deploymentgroupname $deploymentGroupName --auth PAT --token $patToken --adddeploymentgrouptags --deploymentgrouptags $deploymentAgentTags --unattended 
+    }
+    popd
+}
+
+function Remove-InstallFiles
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]$agentInstallPath,
+        [Parameter(Mandatory=$true)]$agentZipFile
+        )
+
+    pushd $agentInstallPath
+    $agentZip="$agentInstallPath\$agentZipFile";
+    Remove-Item $agentZip;
+    popd
+}
+
+
+function Install-DeploymentGroupAgent 
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]$accountUrl,
+        [Parameter(Mandatory=$true)]$projectName,
+        [Parameter(Mandatory=$true)]$deploymentGroupName,
+        [Parameter(Mandatory=$true)]$userName,
+        [Parameter(Mandatory=$true)]$patToken,
+        [Parameter(Mandatory=$false)] [String] [AllowEmptyString()] $deploymentAgentTags,
+        [Parameter(Mandatory=$false)] [String] [AllowEmptyString()] $agentInstallPath,
+        [Parameter(Mandatory=$false)] [String] [AllowEmptyString()] $agentName
+        )
+
+    $ErrorActionPreference="Stop";
+
+    $agentZipFile = "agent.zip"
+
+    #Initial checks
+    Test-InstallPrerequisites
+    
+    #Create agent directory
+    $agentInstallPath = New-AgentPath $agentInstallPath
+    
+    #Download Agent
+    Download-DeploymentGroupAgent $accountUrl $userName $patToken $agentInstallPath $agentZipFile
+
+    #Configure Agent
+    Configure-DeploymentGroupAgent $accountUrl $projectName $deploymentGroupName $patToken $agentName $agentInstallPath $deploymentAgentTags  
+
+    #Cleanup
+    Remove-InstallFiles $agentInstallPath $agentZipFile
+}
+
+Install-DeploymentGroupAgent $accountUrl $projectName $deploymentGroupName $userName $patToken $deploymentAgentTags $agentInstallPath $agentName 
+
+
+
+
+
+
+
+
